16bba8916972dde01c80348f755cd0b69c06e2cd
Martin Rey <m.rey00@gmail.com>
2025-09-04 09:36:26 -0300

    Clase 1 2025 - Demo de persistencia básico

---
+++ Demo-persistencia/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Demo_persistencia
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSeleccionarRuta = new System.Windows.Forms.Button();
+            this.dlgRuta = new System.Windows.Forms.FolderBrowserDialog();
+            this.lblRuta = new System.Windows.Forms.Label();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnAbrir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSeleccionarRuta
+            // 
+            this.btnSeleccionarRuta.Location = new System.Drawing.Point(13, 13);
+            this.btnSeleccionarRuta.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnSeleccionarRuta.Name = "btnSeleccionarRuta";
+            this.btnSeleccionarRuta.Size = new System.Drawing.Size(388, 90);
+            this.btnSeleccionarRuta.TabIndex = 0;
+            this.btnSeleccionarRuta.Text = "Seleccionar ubicación del archivo";
+            this.btnSeleccionarRuta.UseVisualStyleBackColor = true;
+            this.btnSeleccionarRuta.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblRuta
+            // 
+            this.lblRuta.AutoSize = true;
+            this.lblRuta.Location = new System.Drawing.Point(12, 121);
+            this.lblRuta.Name = "lblRuta";
+            this.lblRuta.Size = new System.Drawing.Size(174, 20);
+            this.lblRuta.TabIndex = 1;
+            this.lblRuta.Text = "Ruta no seleccionada.";
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(13, 178);
+            this.btnGuardar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(388, 90);
+            this.btnGuardar.TabIndex = 2;
+            this.btnGuardar.Text = "Guardar los datos de ejemplo";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnAbrir
+            // 
+            this.btnAbrir.Location = new System.Drawing.Point(13, 276);
+            this.btnAbrir.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAbrir.Name = "btnAbrir";
+            this.btnAbrir.Size = new System.Drawing.Size(388, 90);
+            this.btnAbrir.TabIndex = 3;
+            this.btnAbrir.Text = "Abrir y ver contenido del archivo";
+            this.btnAbrir.UseVisualStyleBackColor = true;
+            this.btnAbrir.Click += new System.EventHandler(this.btnAbrir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(679, 484);
+            this.Controls.Add(this.btnAbrir);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.lblRuta);
+            this.Controls.Add(this.btnSeleccionarRuta);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSeleccionarRuta;
+        private System.Windows.Forms.FolderBrowserDialog dlgRuta;
+        private System.Windows.Forms.Label lblRuta;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnAbrir;
+    }
+}
+
---
+++ Demo-persistencia/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Demo_persistencia
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private string path = "";
+        private string[] data = new string[] { "Hola mundo", "Esto es un ejemplo", "de persistencia" };
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (dlgRuta.ShowDialog() == DialogResult.OK)
+            {
+                path = dlgRuta.SelectedPath;
+                lblRuta.Text = $"Ubicación seleccionada: {path}";
+            }
+            
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            string fullPath = Path.Combine(path, "datos.txt");
+            if (!File.Exists(fullPath))
+            {
+                // El archivo no existe
+                using (StreamWriter outputFile = new StreamWriter(fullPath))
+                {
+                    // Escribir el contenido del archivo línea a línea
+                    foreach (var linea in data)
+                    {
+                        outputFile.WriteLine(linea);
+                    }
+                }
+                MessageBox.Show("Archivo creado y datos guardados correctamente.");
+            } 
+            else
+            {
+                // El archivo ya existe, agregamos el mismo contenido
+                using (StreamWriter outputFile = new StreamWriter(fullPath, true))
+                {
+                    // Escribir el contenido del archivo línea a línea
+                    foreach (var linea in data)
+                    {
+                        outputFile.WriteLine($"add: {linea}");
+                    }
+                }
+                MessageBox.Show("Archivo actualizado correctamente.");
+            }
+        }
+
+        private void btnAbrir_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string fullPath = Path.Combine(path, "datos.txt");
+                // Open the text file using a stream reader.
+                using (StreamReader reader = new StreamReader(fullPath))
+                {
+                    // Read the stream as a string.
+                    string text = reader.ReadToEnd();
+
+                    MessageBox.Show(text);
+                }
+
+                
+            }
+            catch (IOException ex)
+            {
+                MessageBox.Show("The file could not be read:");
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
