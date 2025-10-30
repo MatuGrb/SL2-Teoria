ccb38d3205c887e7e26d257f36331248ced012cf
MatuGrb <matiasgra8@gmail.com>
2025-10-30 13:04:38 -0300

    API

---
+++ API/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Vamos a hacer una consulta a la API");
+
+string url = "https://dolarapi.com/v1/dolares/cripto";
+
+HttpClient client = new HttpClient();
+
+try
+{
+    HttpResponseMessage response = await client.GetAsync(url);
+    if (response.IsSuccessStatusCode)
+    {
+        string responseBody = await response.Content.ReadAsStringAsync();
+        Console.WriteLine(responseBody);
+    }
+    else
+    {
+        Console.WriteLine($"Error: {response.StatusCode}");
+        // Console.WriteLine($"Error desc: {response}");
+    }
+    
+}
+catch (HttpRequestException e)
+{
+    Console.WriteLine($"Request error: {e.Message}");    
+}
